d3f3598649426eb1f3963de977a2fad93b79cb57
Mishak Mosimabale <mishakmanuel@gamil.com>
2022-09-08 07:58:04 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /*
  * main - Entry point of the program
- * takes no arg. 
- * 
+ * takes no arg.
+ *
  * Des: used puts() func to print string to stdout
  * Return 0 always
  */
-int main(void)
+int main(void) /*main func*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
