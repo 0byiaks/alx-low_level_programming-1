1b4ba8a66958b7d7214e27fdcc6b6f7154063a4d
Mishak Mosimabale <mishakmanuel@gamil.com>
2022-09-08 07:56:09 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
-
-
-/**
- *
- * main - program start and ends in this function
- *
- * takes no arguments
- *
- *   
- * Description: contains a puts() function to print string to stdout
- *
- * Return: function returns 0
- *
- *       */
-
+/*
+ * main - Entry point of the program
+ * takes no arg. 
+ * 
+ * Des: used puts() func to print string to stdout
+ * Return 0 always
+ */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
