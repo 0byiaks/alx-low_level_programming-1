3a357bd74073e2e08405d1956c383475257fc4db
Mishak Mosimabale <mishakmanuel@gamil.com>
2022-09-08 07:39:04 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/** main  -Entry point
+/*
+ * main  -Entry point
+ * takes no argument
+ *
+ * Description: used a puts() function to print a string to stdout
  * Return: Always 0(success)
  */
 int main(void)
